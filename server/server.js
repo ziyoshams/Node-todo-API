cd8783c27600985a5afb0c169077fc0e42ade8ea
Ziyovuddin <z.shamsiddini@gmail.com>
2017-09-19 12:56:37 -0400

    Added GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -23,6 +24,37 @@ app.post('/todos', (req, res)=>{
 
 });
 
+app.get('/todos', (req, res)=>{
+  Todo.find({}).then((todos)=>{
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(todo){
+      res.send(todo);
+    }
+    else {
+      res.status(404).send();
+    }
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+
 app.listen(3000, ()=>{
   console.log('Started on port 3000');
 });
+
+
+module.exports = {app};
